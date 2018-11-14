2354dd4a5ef9e97749fafc2011a83a3554ee0341
hacky1610 <hacky1610@gmx.de>
2018-11-14 18:17:10 +0100

    Update test.js

--- NodeApp/test/test.js
+++ NodeApp/test/test.js
@@ -1,12 +1,12 @@
 var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1,2,3].indexOf(4), -1);
+describe('Calculator', function() {
+  describe('add', function() {
+    it('should return 2', function() {
+      assert.equal(1+1, 2);
     });
     
-     it('should return 3 when the value is not present', function() {
-      assert.equal([1,2,3,4].indexOf(4), 3);
+     it('should return 3', function() {
+      assert.equal(1+2, 3);
     });
     
   });
